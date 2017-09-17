5a1bbad45a1da25010a7167ac1c0df0a0df08ff1
lilei <lilei@shrbank.com>
2017-09-17 10:37:49 +0800

    新增二分查找法类，新增jar包

---
+++ src/com/shrbank/basic/BinarySearch.java
@@ -0,0 +1,43 @@
+package com.shrbank.basic;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * Created by lilei on 2017/9/17 上午9:46.
+ */
+public class BinarySearch {
+    public static int rank(int key, int[] array) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (key < array[middle]) {
+                high = middle - 1;
+            } else if (key > array[middle]) {
+                low = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int[] whitelist = In.readInts(args[0]);
+            Arrays.sort(whitelist);
+            while (!StdIn.isEmpty()) {
+                int key = StdIn.readInt();
+                if (rank(key, whitelist) == -1) {
+                    StdOut.println(key);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
