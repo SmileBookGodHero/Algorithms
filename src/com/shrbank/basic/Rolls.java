e8e5545d43fd6f0fd30963688b11925f2e63680c
lilei <lilei@shrbank.com>
2017-09-19 15:04:18 +0800

    新增算法实例代码

--- src/com/shrbank/basic/Rolls.java
+++ src/com/shrbank/basic/Rolls.java
@@ -9,7 +9,7 @@ import edu.princeton.cs.algs4.StdRandom;
  */
 public class Rolls {
     public static void main(String[] args) {
-        int T = Integer.parseInt("10");
+        int T = Integer.parseInt("2");
         int SIDES = 6;
         Counter[] rolls = new Counter[SIDES + 1];
         for (int i = 1; i < SIDES; i++) {
