3a7696b9e81c605d49e1350f4eef74109a79468c
lilei <lilei@shrbank.com>
2017-09-20 14:26:13 +0800

    代码格式修改

--- src/com/shrbank/basic/Rolls.java
+++ src/com/shrbank/basic/Rolls.java
@@ -12,15 +12,17 @@ public class Rolls {
         int T = Integer.parseInt("60000");
         int SIDES = 6;
         Counter[] rolls = new Counter[SIDES + 1];
-        for (int i = 1; i <= SIDES; i++)
+        for (int i = 1; i <= SIDES; i++) {
             rolls[i] = new Counter(i + "'s");
+        }
 
         for (int t = 0; t < T; t++) {
             int result = StdRandom.uniform(1, SIDES + 1);
             rolls[result].increment();
         }
-        for (int i = 1; i <= SIDES; i++)
-            StdOut.println(rolls[i]);
 
+        for (int i = 1; i <= SIDES; i++) {
+            StdOut.println(rolls[i]);
+        }
     }
 }
