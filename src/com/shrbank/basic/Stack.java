9bb54ed64d7dd7bfec35a2cd49b5a9b3dc832bec
lilei <lilei@shrbank.com>
2017-09-22 18:18:23 +0800

    新增代码注释

--- src/com/shrbank/basic/Stack.java
+++ src/com/shrbank/basic/Stack.java
@@ -6,36 +6,38 @@ import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Iterator;
 
+
 /**
  * Created by lilei on 2017/9/21 下午1:43.
+ * 下压堆栈（链表实现）
  */
 public class Stack<Item> implements Iterable<Item> {
-    private Node first;
-    private int N;
-    private class Node {
+    private Node first;   //  栈顶（最近添加的元素）
+    private int N;        //  元素数量
+    private class Node {     //  定义了结点的嵌套类
         Item item;
         Node next;
     }
-    public boolean isEmpty() {
+    public boolean isEmpty() {   //  或 N == 0
         return first == null;
     }
     public int size() {
         return N;
     }
-    public void push(Item item) {
+    public void push(Item item) {   //  向栈顶添加元素
         Node oldFirst = first;
         first = new Node();
         first.item = item;
         first.next = oldFirst;
         N++;
     }
-    public Item pop() {
+    public Item pop() {          //  从栈顶删除元素
         Item item = first.item;
         first = first.next;
         N--;
         return item;
     }
-    public Iterator<Item> iterator() {
+    public Iterator<Item> iterator() {   //  迭代
         return new ListIterator();
     }
     public class ListIterator implements Iterator<Item> {
@@ -51,7 +53,7 @@ public class Stack<Item> implements Iterable<Item> {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //  创建一个栈并根据 StdIn 中的指示压入或弹出字符串
         Stack<String> string = new Stack<>();
         while (!StdIn.isEmpty()) {
             String item = StdIn.readString();
