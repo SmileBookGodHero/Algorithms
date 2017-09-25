3fd0291db2a339d6909af19d39dd6677505e869c
李磊 <1198189741@qq.com>
2017-09-25 11:27:43 +0800

    配置文件修改

---
+++ src/com/shrbank/demo/Test05.java
@@ -0,0 +1,14 @@
+package com.shrbank.demo;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by lilei on 2017/9/25 上午11:24.
+ */
+public class Test05 {
+    public static void main(String[] args) {
+        for (String s : args) {
+            StdOut.println(s);
+        }
+    }
+}
