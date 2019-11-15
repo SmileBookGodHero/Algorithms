28296934fc8b603930cb75d9976fe68f2d6e8d11
李磊 <1198189741@qq.com>
2019-11-15 13:25:48 +0800

    测试代码编写

--- src/com/lilei11981/basic/test/TestDiscrete.java
+++ src/com/lilei11981/basic/test/TestDiscrete.java
@@ -8,9 +8,24 @@ package com.lilei11981.basic.test;
 
 public class TestDiscrete {
     public static void main(String[] args) {
-        double[] a = {0.1, 0.2, 0.3, 0.5};
+//        double[] a = {0.1, 0.2, 0.3, 0.5};
 //        int[] a = new int[3];
-        int b = StdRandom.discrete(a);
-        System.out.println(b);
+//        int b = StdRandom.discrete(a);
+//        System.out.println(b);
+//        System.out.println(1/0);
+//        System.out.println(1.0/0.0);
+//        String s = "Hello World";
+//        s = s.toUpperCase();
+//        s = s.substring(6, 11);
+//        System.out.println(s);
+
+//        String string1 = "W";
+//        String string2 = string1;
+//        string1 = "World";
+//        System.out.println(string1);
+//        System.out.println(string2);
+        String a = "Hello World";
+        System.out.println(a.substring(3));
+
     }
 }
