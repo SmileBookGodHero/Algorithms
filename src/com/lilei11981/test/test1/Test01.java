b374d071dca0dcdfe18e428defc2559872d4ac54
李磊 <1198189741@qq.com>
2020-07-18 16:50:44 +0800

    算法代码更新

--- src/com/lilei11981/test/test1/Test01.java
+++ src/com/lilei11981/test/test1/Test01.java
@@ -5,10 +5,11 @@ package com.lilei11981.test.test1;
  */
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println((0 + 15) / 2);
-        System.out.println(2.0e-6 * 100000000.1);
-        System.out.println(true && false || false && true);
-        System.out.println(1 + 2 + "3");
-        System.out.println(1 + 2 + 3 + 4.0);
+//        System.out.println((0 + 15) / 2);
+//        System.out.println(2.0e-6 * 100000000.1);
+//        System.out.println(true && false || false && true);
+//        System.out.println(1 + 2 + "3");
+//        System.out.println(1 + 2 + 3 + 4.0);
+        System.out.println(Math.abs(-2147483648));
     }
 }
